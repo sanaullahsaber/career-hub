44222f589fffa2d108966a36c6c176badd600976
sanaullahsabere <you@example.com>
2023-04-10 00:32:12 +0600

    navbar added with router done

---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Statistics from './components/Statistics';
+import AppliedJobs from './components/AppliedJobs';
+import Blog from './components/Blog';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    children: [
+      {
+        path: "/",
+        element: <Statistics></Statistics>,
+      },
+      {
+        path: "/applied",
+        element: <AppliedJobs></AppliedJobs>
+      },
+      {
+        path: "/blog",
+        element: <Blog></Blog>
+      }
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
