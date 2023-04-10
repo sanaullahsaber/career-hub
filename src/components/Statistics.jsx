47f7349ddfd33cb894bae060e546ee833946ac5f
sanaullahsabere <you@example.com>
2023-04-11 01:38:16 +0600

    statistics section

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,9 +1,75 @@
-import React from 'react';
-
+import React, { PureComponent } from "react";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 const Statistics = () => {
+  const assignmentMarks = [
+    {
+      id: 1,
+      name: "Assignment 1",
+      mark: 57,
+    },
+
+    {
+      id: 2,
+      name: "Assignment 2",
+      mark: 57,
+    },
+
+    {
+      id: 3,
+      name: "Assignment 3",
+      mark: 60,
+    },
+
+    {
+      id: 4,
+      name: "Assignment 4",
+      mark: 53,
+    },
+
+    {
+      id: 5,
+      name: "Assignment 5",
+      mark: 39,
+    },
+
+    {
+      id: 6,
+      name: "Assignment 6",
+      mark: 54,
+    },
+
+    {
+      id: 7,
+      name: "Assignment 7",
+      mark: 60,
+    },
+
+    {
+      id: 8,
+      name: "Assignment 8",
+      mark: 60,
+    },
+  ];
   return (
-    <div>
-      this is statistics page
+    <div className="my-container">
+      <ResponsiveContainer width="100%" aspect={3}>
+        <AreaChart width={1000} height={300} data={assignmentMarks}>
+          <Area dataKey="name"></Area>
+          <Area stroke="#8884d8" dataKey="mark"></Area>
+          <CartesianGrid strokeDasharray="3 3"></CartesianGrid>
+          <XAxis dataKey="name"></XAxis>
+          <YAxis></YAxis>
+          <Tooltip></Tooltip>
+        </AreaChart>
+      </ResponsiveContainer>
     </div>
   );
 };
